b252ff9c8aca37217a0eb1ac7cf797ab71cb4775
Eduard Ungureanu <u.eduard.email@gmail.com>
2023-06-01 10:13:57 +0300

    Add dependency for WP Deubbing plugin

--- index.php
+++ index.php
@@ -4,25 +4,37 @@
  * @version 1.0.0
  */
 /*
-Plugin Name: Et Debug
+Plugin Name: ET Debug
 Plugin URI: https://github.com/eduard-ungureanu/et-debug
-Description: This is an add-on for the original WP Debuggin Plugin, which adds the option to enable/disable the `ET_DEBUG` constant which will enable the debugging for submittions process of Email Opt-in module and Bloom 
+Description: Adds the <strong>ET_DEBUG</strong> constant if the <strong>WP Debugging</strong> plugin is active.
 Author: Eduard Ungureanu
 Version: 1.0.0
 Author URI: https://github.com/eduard-ungureanu
 */
+add_action('plugins_loaded', function () {
+	require_once(ABSPATH . '/wp-admin/includes/plugin.php');
 
-add_filter(
-	'wp_debugging_add_constants',
-	function( $added_constants ) {
-		$my_constants = [
-			'my_test_constant'      => [
-				'ET_DEBUG' => 'true',
-				'raw'   => false,
-			],
-		];
-		return array_merge( $added_constants, $my_constants );
-	},
-	10,
-	1
-);
+	if (!is_plugin_active('wp-debugging/wp-debugging.php')) {
+		deactivate_plugins('et-debug/index.php');
+		add_action('admin_notices', 'dt_admin_notice');
+  } else {
+		add_filter('wp_debugging_add_constants', function( $added_constants ) {
+			$etdebug = [
+				'et_debug'      => [
+					'value' => 'true',
+					'raw'   => true,
+				],
+			];
+		return array_merge( $added_constants, $etdebug );
+		}, 10, 1);
+	}
+});
+
+/**
+ * dt_admin_notice
+ *
+ * @return void
+ */
+function dt_admin_notice() {
+	echo '<div class="error"><p>Plugin deactivated. Please activate <strong>WP Debugging Plugin first</strong>!</p></div>';
+}
