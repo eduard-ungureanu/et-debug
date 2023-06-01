06a67aa37cc9546b557304d21bbfaf1a93bec26e
Eduard Ungureanu <u.eduard.email@gmail.com>
2023-06-01 09:38:41 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package ET_DEBUG
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Et Debug
+Plugin URI: https://github.com/eduard-ungureanu/et-debug
+Description: This is an add-on for the original WP Debuggin Plugin, which adds the option to enable/disable the `ET_DEBUG` constant which will enable the debugging for submittions process of Email Opt-in module and Bloom 
+Author: Eduard Ungureanu
+Version: 1.0.0
+Author URI: https://github.com/eduard-ungureanu
+*/
+
